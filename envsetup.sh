7d9b46c0a1b9281202f17f15604e4191098cce31
Tushar Joshi <tusharvjoshi@gmail.com>
2025-06-28 13:27:18 +0530

    Add env setup commands

---
+++ envsetup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -LO https://github.com/roboll/helmfile/releases/download/v0.135.0/helmfile_linux_amd64
+mv helmfile_linux_amd64 helmfile
+chmod 777 helmfile
+sudo mv helmfile /usr/local/bin
